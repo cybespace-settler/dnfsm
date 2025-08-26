9ad94af6bc3d9e2ce7e4237529088e1386b790fe
cyberspace-settler <aifistinganal@proton.me>
2025-08-26 15:21:30 +0100

    Initial commit

---
+++ dnfsm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Updating system"
+dnf update && dnf upgrade || echo "Something with updates(("
+echo "Removing unneeded dependencies"
+dnf autoremove || echo "Oops, autoremove doesn't work" 
+echo "Cleaning dnf garbage"
+dnf clean all || echo "Dnf garbage cleaning failed"
+echo "You want to remove .cache?"
+read -r resp
+if [[ "$resp" == "y" ]]; then
+ rm -rf ~/.cache
+ echo ".cache cleaned"
+else
+ echo "Your choice"
+fi
+echo "Done"
