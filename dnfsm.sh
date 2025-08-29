1d1aaab7b3fc537f939b83ea3e9769c4535758b9
cyberspace-settler <aifistinganal@proton.me>
2025-08-29 14:11:41 +0100

    Fixed --force

--- dnfsm.sh
+++ dnfsm.sh
@@ -6,11 +6,11 @@ dnf autoremove || echo "Oops, autoremove doesn't work"
 echo "Cleaning dnf garbage"
 dnf clean all || echo "Dnf garbage cleaning failed"
 echo "You want to remove .cache?"
-read -r resp
 if [[ "$1" == "--force" ]]; then
     rm -rf ~/.cache/*
     echo ".cache cleaned (force mode)"
 else
+ read -r response
  if [[ "$resp" == "y" ]]; then
   rm -rf ~/.cache/*
   echo ".cache cleaned"
