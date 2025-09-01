1c1f0e6a34de908bb7c4fc756172b7361a49787f
cyberspace-settler <aifistinganal@proton.me>
2025-09-01 13:08:19 +0100

    Added aptsm.sh for debian based systems support

---
+++ aptsm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Updating system"
+apt update -y && apt upgrade -y || echo "dnf update doesnt work, maybe network idk"
+echo "Removing unneeded dependencies"
+dnf autoremove || echo "Oops, autoremove doesn't work"
+echo "Cleaning dnf garbage"
+dnf clean all || echo "Dnf garbage cleaning failed"
+echo "You want to remove .cache?"
+if [[ "$1" == "--force" ]]; then
+    rm -rf ~/.cache/*
+    echo ".cache cleaned (force mode)"
+else
+ read -r response
+ if [[ "$resp" == "y" ]]; then
+  rm -rf ~/.cache/*
+  echo ".cache cleaned"
+ else
+  echo "Your choice"
+ fi
+fi
+echo "Empty storage space on root: $(df -h /)"
